a58daf4b8da1e81a4dffbf1639451e3daf8b5fdb
Kevin L <kevin@T430.(none)>
2014-01-30 14:16:58 -0500

    Parser is happening

---
+++ Project1/netlist.h
@@ -0,0 +1,32 @@
+typedef int wire_n; 
+typedef struct port { 
+    char * name; 
+    wire_n wire; 
+    int x; 
+    int y;  
+} port_t; 
+typedef struct gate {
+    char * name; 
+    wire_n fanin; 
+    wire_n fanout; 
+    int x; 
+    int y;
+} gate_t; 
+typedef struct wire { 
+    gate_t ** gates; 
+    port_t ** ports; 
+    char * name; 
+    int num_gates; 
+    int num_ports; 
+} wire_t; 
+typedef struct layout {
+    port_t ** all_ports; 
+    gate_t ** all_gates; 
+    wire_t ** all_wires; 
+    int x_size; 
+    int y_size; 
+    int size_gates; 
+    int size_ports; 
+    int size_wires; 
+} layout_t;
+
--- Project1/parser.c
+++ Project1/parser.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "netlist.h"
+#include "parser.h"
+
 int main() {
     FILE * netlist_f;
+    layout_t * layout = calloc(1,sizeof(layout_t));
     netlist_f = fopen("netlist.txt","r");
     if (netlist_f) {
         char *buffer = 0; 
         size_t buflen = 0; 
-        while (getline(&buffer, &buflen, netlist_f)) != -1) {
-            printf(buffer);
+        char ** tokens; 
+        getline(&buffer, &buflen, netlist_f);
+        tokens = parser_split(buffer, ' ');
+        parser_createLayout(layout, tokens);
+        while (getline(&buffer, &buflen, netlist_f) != -1) {
+            tokens = parser_split(buffer, ' ');
+            switch (tokens[0][0]) {
+                case 'p':
+                    break;
+                case 'g':
+                    break;
+                default:
+                    printf("Something went wrong in parser land"); 
+            }
+            printf("%s\n",tokens[0]);
         }
     } else {
         printf ("No valid netlist found\n");
     }
     return 0;
-
 }
