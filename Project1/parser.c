197428a1fb777b27b439c5b22496cf7019c24e3b
Kevin L <kevin@T430.(none)>
2014-02-02 18:05:43 -0500

    Gates link to wires

--- Project1/parser.c
+++ Project1/parser.c
@@ -1,10 +1,100 @@
 #include "netlist.h"
 #include "parser.h"
 #include "common.h"
+/* 
+* Called on the first line of parsing, provides a layout construct
+* Layout construct (layout_t) is a structure that servers the purpose
+* of a layout class in an OOP
+* Holds all gates/ports/wires
+* Also manages the grid that these objects live in
+* Will likely be exported as LEF/DEF
+*/ 
+layout_t* parser_createLayout(layout_t * layout, char ** init) {
+    layout->x_size = atoi(init[0]);
+    layout->y_size = atoi(init[1]);
+    layout->size_wires = atoi(init[2]); 
+    layout->size_gates = atoi(init[3]); 
+    layout->size_ports = atoi(init[4]);
+    layout->all_ports = calloc(layout->size_ports, sizeof(port_t)); 
+    layout->all_gates = calloc(layout->size_gates, sizeof(gate_t)); 
+    layout->all_wires = calloc(layout->size_wires, sizeof(wire_t)); 
+    int i; 
+    for (i = 0; i < layout->size_wires; i++) {
+        char ** buffer= &(layout->all_wires[i].name);
+        *buffer = malloc(i/10+2); 
+        sprintf(*buffer,"%d",i);
+    } 
+    assert(layout->all_ports);
+    assert(layout->all_wires);
+    assert(layout->all_gates);
+    layout->size_ports = 0; 
+    layout->size_gates = 0; 
+    return layout;
+}
+/* 
+* Link gates to wires in layout graph
+*/
+void parser_linkGate(layout_t *layout, wire_n wiren, gate_n gaten) {
+    wire_t * wire = &(layout->all_wires[wiren]); 
+    int sz = wire->num_gates + 1; 
+    gate_n * new_gate_bfr = malloc(sizeof(gate_n) * sz);  
+    //memcpy of 0 bytes is actually undefined behavior
+    //But I can't see why std libs would break this
+    memcpy(new_gate_bfr,wire->gates,sizeof(gate_n) * (sz-1)); 
+    new_gate_bfr[sz-1] = gaten; 
+    wire->gates = new_gate_bfr;
+    wire->num_gates += 1;  
+}
+/*
+* Parse any line that has gates
+* add to layout and link gates to wires
+*/
+layout_t* parser_addGate(layout_t *layout, char ** init) {
+    int i = 1; 
+    int fanin_size = 0; 
+    wire_n * fanin_bfr = malloc(sizeof(wire_n)*20);
+    gate_t * self_ptr = &(layout->all_gates[layout->size_gates]); 
+    while (init[i]) { 
+        if (strstr(init[i],"name=")) {
+            int t_len = strlen("name="); 
+            int size = strlen(init[i]) - t_len; 
+            char ** t_ptr = &(self_ptr->name);
+            *t_ptr = malloc(size); 
+            memcpy(*t_ptr, init[i] + t_len, size);  
+        } else if (strstr(init[i],"fanin=")) {
+            assert(fanin_size < 20); 
+            int t_len = strlen("fanin="); 
+            fanin_bfr[fanin_size] = atoi(init[i]+t_len);
+            parser_linkGate(layout,fanin_bfr[fanin_size],layout->size_gates); 
+            fanin_size++;
+        } else if (strstr(init[i],"fanout=")) {
+            int t_len = strlen("fanout="); 
+            wire_n * fi = &(self_ptr->fanout);
+            *fi = atoi(init[i]+t_len);
+            parser_linkGate(layout,*fi,layout->size_gates); 
+        } 
+        i++;
+    }
+    self_ptr->fanin = malloc(sizeof(wire_n)*fanin_size); 
+    self_ptr->fanin_size = fanin_size;
+    memcpy(self_ptr->fanin, fanin_bfr, fanin_size * sizeof(wire_n));
+    free(fanin_bfr);
+    layout->size_gates += 1;
+    return layout; 
+}
+
+layout_t* parser_addPort(layout_t *layout, char ** init) {
+    return layout; 
+}
+/*
+*TODO: allow for wires to take on names
+*/
+layout_t* parser_addWire(layout_t *layout, char ** init) {
+    return layout; 
+}
 /*str_spit shamelessly stolen from 
 *http://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
 */
-
 char** parser_split(char* a_str, const char a_delim)
 {
     char** result    = 0;
@@ -35,67 +125,4 @@ char** parser_split(char* a_str, const char a_delim)
         *(result + idx) = 0;
     }
     return result;
-}
-
-layout_t* parser_createLayout(layout_t * layout, char ** init) {
-    layout->x_size = atoi(init[0]);
-    layout->y_size = atoi(init[1]);
-    layout->size_wires = atoi(init[2]); 
-    layout->size_gates = atoi(init[3]); 
-    layout->size_ports = atoi(init[4]);
-    layout->all_ports = calloc(layout->size_ports, sizeof(port_t)); 
-    layout->all_gates = calloc(layout->size_gates, sizeof(gate_t)); 
-    layout->all_wires = calloc(layout->size_wires, sizeof(wire_t)); 
-    int i; 
-    for (i = 0; i < layout->size_wires; i++) {
-        char ** buffer= &(layout->all_wires[i].name);
-        *buffer = malloc(i/10+2); 
-        sprintf(*buffer,"%d",i);
-    } 
-    assert(layout->all_ports);
-    assert(layout->all_wires);
-    assert(layout->all_gates);
-    layout->size_ports = 0; 
-    layout->size_gates = 0; 
-    return layout;
-}
-
-layout_t* parser_addGate(layout_t *layout, char ** init) {
-    int i = 1; 
-    int fanin_size = 0; 
-    wire_n * fanin_bfr = malloc(sizeof(wire_n)*20);
-    gate_t * self_ptr = &(layout->all_gates[layout->size_gates]); 
-    while (init[i]) { 
-        if (strstr(init[i],"name=")) {
-            int t_len = strlen("name="); 
-            int size = strlen(init[i]) - t_len; 
-            char ** t_ptr = &(self_ptr->name);
-            *t_ptr = malloc(size); 
-            memcpy(*t_ptr, init[i] + t_len, size);  
-        } else if (strstr(init[i],"fanin=")) {
-            assert(fanin_size < 20); 
-            int t_len = strlen("fanin="); 
-            fanin_bfr[fanin_size] = atoi(init[i]+t_len);
-            fanin_size++; 
-        } else if (strstr(init[i],"fanout=")) {
-            int t_len = strlen("fanout="); 
-            wire_n * fi = &(self_ptr->fanout);
-            *fi = atoi(init[i]+t_len);
-        } 
-        i++;
-    }
-    self_ptr->fanin = malloc(sizeof(wire_n)*fanin_size); 
-    self_ptr->fanin_size = fanin_size;
-    memcpy(self_ptr->fanin, fanin_bfr, fanin_size * sizeof(wire_n));
-    free(fanin_bfr);
-    layout->size_gates += 1;
-    return layout; 
-}
-
-layout_t* parser_addPort(layout_t *layout, char ** init) {
-    return layout; 
-}
-
-layout_t* parser_addWire(layout_t *layout, char ** init) {
-    return layout; 
 }
