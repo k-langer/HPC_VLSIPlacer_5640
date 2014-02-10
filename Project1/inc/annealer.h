f0e76ab11bfcdcdf79505e2517394b2df40125b4
Kevin L <kevin@T430.(none)>
2014-02-10 00:52:49 -0500

    annealer

--- Project1/inc/annealer.h
+++ Project1/inc/annealer.h
@@ -5,4 +5,6 @@
 layout_t * annealer_createInitialPlacement(layout_t * );
 layout_t * annealer_anneal(layout_t * layout, int wirelength);
 gate_n * annealer_swapGates(layout_t * , gate_n, coord_t);
+layout_t * annealer_simulatedAnnealing(layout_t *, int, double);
+bool_t annealer_acceptSwap(int , double);
 #endif
