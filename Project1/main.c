819b0275024325eb4646da8264fea9d2cc7f1f25
Kevin L <kevin@T430.(none)>
2014-03-11 05:56:30 -0400

    timing updated

--- Project1/main.c
+++ Project1/main.c
@@ -36,7 +36,7 @@ void production(layout_t * layout) {
 void benchmark(layout_t * layout, char * name) {
     struct timeval start, end;
     for (int iterations = 0; iterations < 5; iterations++) {
-        for (int threads = 0; threads < 16; threads++) {
+        for (int threads = 1; threads < 16; threads++) {
             layout_t *layout = parser_parseNetlist(name); 
             annealer_createInitialPlacement(layout);
             int sum1 = netlist_layoutWirelength(layout);
@@ -46,8 +46,8 @@ void benchmark(layout_t * layout, char * name) {
             int sum = netlist_layoutWirelength(layout);
             long int sec = ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec));
             netlist_free(layout);
-            printf("%d\t%ld\t%d\t%d\n",threads,sec,sum,sum1);
-            netlist_free(layout);
+            printf("%d\t%ld\t%ld\t%d\t%d\n",threads,sec/1000000,sec,sum,sum1);
+            //netlist_free(layout);
         }
     }
 }
