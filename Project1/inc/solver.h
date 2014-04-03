e86f9f2722f24fc2c431e6c3160fd9e0203ae5e3
Kevin L <kevin@T430.(none)>
2014-04-03 06:22:57 -0400

    solver start

--- Project1/inc/solver.h
+++ Project1/inc/solver.h
@@ -1,10 +1,11 @@
-#ifdef __solver__h
 #define __solver__h
+#ifdef __solver__h
 /*
 * The analytic quadratic wire length solver
 * Algorithm ideas from coursera's class:
 * "VLSI CAD: Logic to Layout" 
 * Code written by Langer for HPC
 */
-gate_t ** solver_solve(layout_t *);
+void solver_addGates(layout_t *, float **);
+void solver_solve(layout_t *);
 #endif 
--- Project1/solver.c
+++ Project1/solver.c
@@ -1,6 +1,36 @@
 #include "netlist.h"
 #include "common.h"
 //TODO: Implement numeric solver to get future GPU speedup 
-gate_t ** solver_solve(layout_t * layout) {
-    return NULL;
+
+void solver_combinatoricsWire(wire_t * wire, float ** C_matrix) {
+    int wc = wire->num_gates; 
+    int x,y; 
+    if( wc < 2 ) {
+       return;  
+    } 
+    float weight = 1 / (wc + 0.0); 
+    printf("%s %d\n",wire->name,wc); 
+    for (int i = 0; i < wc-1; i++) {
+        for (int j = 1; j < wc; j++) {
+            if (i != j) {
+                x = wire->gates[i]; 
+                y = wire->gates[j]; 
+                printf("\t(%d,%d)\n",x,y); 
+                //C_matrix[0][0] = weight;
+                //C_matrix[0][0] = weight;  
+            }
+        }
+    }
+}
+void solver_addGates(layout_t * layout, float **  C_matrix) {
+   for (int wireCount = 0; wireCount < layout->size_wires; wireCount++) {
+       wire_t * wire = layout->all_wires + wireCount; 
+       solver_combinatoricsWire(wire,C_matrix);
+    } 
+}
+void solver_solve(layout_t * layout) {
+    int size_gates = layout->size_gates; 
+    float C_matrix[size_gates][size_gates];
+    solver_addGates(layout,(float **)C_matrix); 
+    printf("%d \n",layout->x_size); 
 }
